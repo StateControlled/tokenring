47f64ec7f0fd1def5d44c5109184f578633bbae0
William <william.berthouex@gmail.com>
2025-06-09 20:20:01 -0500

    Writeup update

--- src/main/scala/application/util/CustomFileReader.scala
+++ src/main/scala/application/util/CustomFileReader.scala
@@ -1,23 +1,23 @@
 package application.util
 
-import application.core.*
-
-import scala.io.Source
-
-object CustomFileReader extends App {
-    readData()
-
-    private def readData(): Unit = {
-        // reads from src/main/resources
-        val resource = Source.fromResource("events.json")
-
-        // file contents to string
-        val builder: StringBuilder = new StringBuilder()
-        resource.getLines().foreach(line => builder.append(line))
-        val jsonString: String = builder.toString()
-
-        // deserialize json string
-        val list: List[Event] = upickle.default.read[List[Event]](jsonString)
-        list.foreach(event => println(event))
-    }
-}
+//import application.core.*
+//
+//import scala.io.Source
+//
+//object CustomFileReader extends App {
+//    readData()
+//
+//    private def readData(): Unit = {
+//        // reads from src/main/resources
+//        val resource = Source.fromResource("events.json")
+//
+//        // file contents to string
+//        val builder: StringBuilder = new StringBuilder()
+//        resource.getLines().foreach(line => builder.append(line))
+//        val jsonString: String = builder.toString()
+//
+//        // deserialize json string
+//        val list: List[Event] = upickle.default.read[List[Event]](jsonString)
+//        list.foreach(event => println(event))
+//    }
+//}
